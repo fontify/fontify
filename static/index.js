5fe0762c00c68f3f214fed56032af19ae63f4c8a
jnijeremy <nizeyu123@gmail.com>
2016-02-28 11:30:06 -0800

    Update dropzone for invalid or multiple uploads

--- static/index.js
+++ static/index.js
@@ -1,3 +1,4 @@
+$("#submit-all").attr("disabled", true);
 $("#submit-all").hide();
 
 Dropzone.options.uploadDropzone = {
@@ -9,14 +10,30 @@ Dropzone.options.uploadDropzone = {
       myDropzone.processQueue();
     });
 
+    this.on("error", function(file, errorMessage) {
+      $("#upload-file-validation").text("Please upload only jpg, png or jpeg file");
+      $("#upload-file-validation").addClass("alert");
+      this.removeFile(file);
+      $("#submit-all").hide();
+    });
+
     this.on("addedfile", function() { 
       $("#submit-all").show();
+      $("#upload-file-validation").text("Upload the scanned template");
+      $("#upload-file-validation").removeClass("alert");
     });
 
     this.on("success", function(file, response) {
-      console.log(response);
       window.location="/finish?key=" + response.key + "&font-name=" + response.font_name;
     });
+
+    this.on("maxfilesexceeded", function(file) {
+      $("#upload-file-validation").text("Upload the scanned template");
+      $("#upload-file-validation").removeClass("alert");
+      this.removeAllFiles();
+      this.addFile(file);
+    });
+
   },
   paramName: "file", // The name that will be used to transfer the file
   maxFilesize: 20, // MB
