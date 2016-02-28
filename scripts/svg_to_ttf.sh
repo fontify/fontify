c6bc637ae25603c83e1b87cde683a2b6676d2011
Hexcles Ma <bob1211@gmail.com>
2016-02-28 02:41:18 -0800

    fix up again

--- scripts/svg_to_ttf.sh
+++ scripts/svg_to_ttf.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-python gen_metadata.py $1 $2 $3 > metadata.json
-third-party/svgs2ttf/svgs2ttf metadata.json
+python gen_metadata.py $1 $2 $3 > $3/metadata.json
+third-party/svgs2ttf/svgs2ttf $3/metadata.json
