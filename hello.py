e8c8424b03904edb9c03685af13e528afd243a81
jnijeremy <nizeyu123@gmail.com>
2016-02-27 18:45:50 -0800

    Added Dropzone in index.html

--- hello.py
+++ hello.py
@@ -1,18 +1,20 @@
-from flask import Flask
-from flask import make_response
-from flask import render_template
+import os
+from flask import Flask, request, redirect, url_for, render_template
 from pdfkit import from_string
 from data import get_chars
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = '/tmp'
+ALLOWED_EXTENSIONS = set(['jpg', 'png'])
 
 app = Flask(__name__)
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def index():
     return render_template(
         'index.html'
     )
-
 
 @app.route("/template")
 def template():
@@ -25,6 +27,23 @@ def template():
     response.headers['Content-Disposition'] = "filename=template.pdf"
     response.mimetype = 'application/pdf'
     return response
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route("/file-upload", methods=['POST'])
+def file_upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # return redirect(url_for('uploaded_file',
+                                    # filename=filename))
+            return "save sucess"
+    return "lalala"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/index.js
@@ -0,0 +1,11 @@
+// "myAwesomeDropzone" is the camelized version of the HTML element's ID
+Dropzone.options.myAwesomeDropzone = {
+  paramName: "file", // The name that will be used to transfer the file
+  maxFilesize: 2, // MB
+  accept: function(file, done) {
+    if (file.name == "justinbieber.jpg") {
+      done("Naha, you don't.");
+    }
+    else { done(); }
+  }
+};
