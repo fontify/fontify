8c8b3a549aa71667de36ecc23c5e0b484da775b4
Svenja Cao <c.svenjax@gmail.com>
2016-02-27 21:27:03 -0800

    Fix ljust last row

--- data.py
+++ data.py
@@ -17,7 +17,7 @@ def get_flat_chars():
 def get_chars():
     chars = get_flat_chars()
     result = [chars[i:i + COLUMNS] for i in xrange(0, len(chars), COLUMNS)]
-    result[-1] = result[-1].ljust(10)
+    result[-1] = result[-1].ljust(COLUMNS)
     return result
 
 
