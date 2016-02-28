b41183bbc23d8a26a1af7f79ba4afe2c28173da2
Hexcles Ma <bob1211@gmail.com>
2016-02-28 02:50:41 -0800

    python2

--- scripts/svg_to_ttf.sh
+++ scripts/svg_to_ttf.sh
@@ -3,5 +3,5 @@
 # $1 font name
 # $2 input directory
 # $3 output ttf
-python gen_metadata.py $1 $2 $3 > $2/metadata.json
+python2 gen_metadata.py $1 $2 $3 > $2/metadata.json
 third-party/svgs2ttf/svgs2ttf $2/metadata.json
