5e16d40605bf5129ce536089865ebcba37a514a8
Hexcles Ma <bob1211@gmail.com>
2016-02-28 02:42:45 -0800

    fix your f up

--- scripts/svg_to_ttf.sh
+++ scripts/svg_to_ttf.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-python gen_metadata.py $1 $2 $3 > $3/metadata.json
-third-party/svgs2ttf/svgs2ttf $3/metadata.json
+# $1 font name
+# $2 input directory
+# $3 output ttf
+python gen_metadata.py $1 $2 $3 > $2/metadata.json
+third-party/svgs2ttf/svgs2ttf $2/metadata.json
